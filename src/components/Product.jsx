f2265df446f99cf53385a5cf5926afa8aee953ba
Keizh_still_beating <krishnamulay05@gmail.com>
2024-03-05 17:53:26 +0530

    import asset video

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,33 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Button from "./Button";
 
 // setPos((pos) => index * 23);
 
-function Product({ name, description, live, pcase, pos, setPos, index }) {
+function Product({ name, description, live, pcase, pos, setPos, index, bg }) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div
-      onMouseOver={() => setPos((pos) => index * 23)}
+      onMouseOver={() => {
+        setPos((pos) => index * 23);
+        setIsHovered(true);
+      }}
+      onMouseOut={() => setIsHovered(false)}
       className="w-full h-fit"
+      style={{ backgroundImage: isHovered ? `url(${bg})` : "" }}
     >
       <div className="max-w-screen-xl h-[23rem] mx-auto flex justify-between items-center text-zinc-200 py-[3rem] ">
         <h1 className="text-6xl">{name}</h1>
         <div className="flex flex-col w-[300px]">
           <p className="mb-4 text-base">{description}</p>
           <div className="flex gap-[10px]">
-            {live && <Button title="Live Website" />}
-            {pcase && <Button title="CaseStudy" />}
+            {isHovered && (
+              <>
+                {live && <Button title="Live Website" />}
+                {pcase && <Button title="Case Study" />}
+              </>
+            )}
           </div>
         </div>
       </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,15 @@ import Product from "./Product";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
+import arqitel from "../assets/Arqitel project video 4_3.webm";
+import cula from "../assets/Cula_promo_new_4_3.mp4";
+import maniv from "../assets/Maniv-Compressed.mp4";
+import summon from "../assets/summon.webm";
+import yahoo from "../assets/yahoo.webm";
+import weglot from "../assets/weglot.web";
+import candid from "../assets/Candid Health 4_3_H.264.webm";
+import showcase from "../assets/showcase_4_3.mp4";
+
 function Products() {
   const products = [
     {
@@ -9,6 +18,7 @@ function Products() {
       description:
         "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into realestate.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c621a3aa0ee3ae147d692_arqitel-bg-p-2000.webp",
       case: false,
     },
     {
@@ -16,6 +26,7 @@ function Products() {
       description:
         "We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b41319bda140bccae204ae_bg%202.png",
       case: false,
     },
     {
@@ -23,6 +34,7 @@ function Products() {
       description:
         "A global early-stage venture fund partnering with founders to advance cleaner, safer, and more sustainable movement of people and goods.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b2ccf5af18668cfbd3a8d6_bg%201.png",
       case: false,
     },
     {
@@ -30,6 +42,7 @@ function Products() {
       description:
         "Our second year was filled with great events, exciting projects, awards and amazing people - so we made another showcase to celebrate.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/6450586e80fac7eb1655014f_yir2022%E2%80%93bg-p-2000.png",
       case: false,
     },
     {
@@ -37,6 +50,7 @@ function Products() {
       description:
         "We enhanced the New York Fashion Week, by creating a fully digital AR  fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models  and an integrated web shop.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/635299cbb5dc5fd8b2972010_Yahoo%20bg.png",
       case: true,
     },
     {
@@ -44,6 +58,7 @@ function Products() {
       description:
         "We enhanced the New York Fashion Week, by creating a fully digital AR  fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models  and an integrated web shop.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/635299d5545a912e9e4a9cb4_Weglot%20for%20Germany%20bg.png",
       case: true,
     },
     {
@@ -51,6 +66,7 @@ function Products() {
       description:
         "We enhanced the New York Fashion Week, by creating a fully digital AR  fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models  and an integrated web shop.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/64b7dbf5f12bc6fafbfb90fa_Candid%20Work%20Background.jpg",
       case: true,
     },
     {
@@ -58,6 +74,7 @@ function Products() {
       description:
         "We enhanced the New York Fashion Week, by creating a fully digital AR  fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models  and an integrated web shop.",
       live: true,
+      bg: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/6352999803e7fe5651137f1e_Showcase%20Website%20bg.png",
       case: true,
     },
   ];
@@ -76,6 +93,7 @@ function Products() {
           pos={pos}
           setPos={setPos}
           index={index}
+          bg={product.bg}
         />
       ))}
       <div className="absolute w-full h-full top-0  pointer-events-none">
@@ -83,67 +101,88 @@ function Products() {
           initial={{ y: 0 }}
           animate={{ y: `${pos}rem` }}
           transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.4 }}
-          className="WINDOW h-[23rem] w-[32rem] absolute bg-sky-100 left-[38%] overflow-hidden rounded-lg"
+          className="WINDOW h-[23rem] w-[32rem] absolute  left-[38%] overflow-hidden rounded-lg "
         >
+          {/* first */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full "
           >
             <video autoPlay loop muted className="object-contain">
-              <source
-                src="src\assets\Arqitel project video 4_3.webm"
-                type="video/mp4"
-              />
+              <source src={arqitel} type="video/mp4" />
             </video>
           </motion.div>
+          {/*second */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full "
           >
             <video autoPlay loop muted className="object-contain">
-              <source
-                src="src\assets\Cula_promo_new_4_3.mp4"
-                type="video/mp4"
-              />
+              <source src={cula} type="video/mp4" />
             </video>
           </motion.div>
+          {/* third */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
-            className="w-full h-full bg-sky-300"
+            className="w-full h-full "
           >
-            {" "}
             <video autoPlay loop muted className="object-contain">
-              <source src="src\assets\Maniv-Compressed.mp4" type="video/mp4" />
+              <source src={maniv} type="video/mp4" />
             </video>
           </motion.div>
+          {/* forth */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full bg-sky-400"
-          ></motion.div>
+          >
+            <video autoPlay loop muted className="object-contain">
+              <source src={summon} type="video/mp4" />
+            </video>
+          </motion.div>
+          {/* fifth */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full bg-sky-100"
-          ></motion.div>
+          >
+            <video autoPlay loop muted className="object-contain">
+              <source src={yahoo} type="video/mp4" />
+            </video>
+          </motion.div>
+          {/* sixth */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full bg-sky-200"
-          ></motion.div>
+          >
+            <video autoPlay loop muted className="object-contain">
+              <source src={weglot} type="video/mp4" />
+            </video>
+          </motion.div>
+          {/* seventh */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full bg-sky-300"
-          ></motion.div>
+          >
+            <video autoPlay loop muted className="object-contain">
+              <source src={candid} type="video/mp4" />
+            </video>
+          </motion.div>
+          {/* eigth */}
           <motion.div
             initial={{ y: 0 }}
             animate={{ y: `-${pos}rem` }}
             className="w-full h-full bg-sky-400"
-          ></motion.div>
+          >
+            <video autoPlay loop muted className="object-contain">
+              <source src={showcase} type="video/mp4" />
+            </video>
+          </motion.div>
         </motion.div>
       </div>
     </div>
